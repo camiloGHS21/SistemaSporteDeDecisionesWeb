211014edf3d8532fda5b12499ccbd16568588695
camiloghs21 <camiloghs21@gmail.com>
2025-10-02 16:47:02 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -44,41 +44,4 @@ createRoot(document.getElementById('root')).render(
       <RouterProvider router={router} />
     </AuthProvider>
   </StrictMode>,
-)
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './index.css'
-import Login from '@pages/Login'
-import Register from '@pages/Register'
-import Comparar from '@pages/Comparar'
-import ProtectedRoute from './components/auth/ProtectedRoute';
-import { AuthProvider } from './context/AuthContext';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Login />,
-  },
-  {
-    path: '/register',
-    element: <Register />,
-  },
-  {
-    element: <ProtectedRoute />,
-    children: [
-      {
-        path:'/comparar',
-        element: <Comparar/>
-      }
-    ]
-  }
-])
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
-  </StrictMode>,
-)
+);
